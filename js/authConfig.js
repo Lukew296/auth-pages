73a9f8e731031ebfefb3640003d602befcb5d56a
Lukew296 <lukew296@proton.me>
2025-11-14 13:21:12 +0000

    Test 1

---
+++ js/authConfig.js
@@ -0,0 +1,10 @@
+// AUTH PROJECT CONFIG (FOR SIGNUP + LOGIN)
+const authConfig = {
+    apiKey: "YOUR_AUTH_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "YOUR_AUTH_DB_URL",
+    projectId: "YOUR_AUTH_PROJECT_ID",
+    storageBucket: "YOUR_AUTH_BUCKET",
+    messagingSenderId: "YOUR_AUTH_SENDER",
+    appId: "YOUR_AUTH_APP_ID"
+};
---
+++ js/chatConfig.js
@@ -0,0 +1,10 @@
+// CHAT PROJECT CONFIG (SEPARATE DATABASE)
+const chatConfig = {
+    apiKey: "YOUR_CHAT_API_KEY",
+    authDomain: "YOUR_CHAT_DOMAIN",
+    databaseURL: "YOUR_CHAT_DB_URL",
+    projectId: "YOUR_CHAT_PROJECT_ID",
+    storageBucket: "YOUR_CHAT_BUCKET",
+    messagingSenderId: "YOUR_CHAT_SENDER",
+    appId: "YOUR_CHAT_APP_ID"
+};
